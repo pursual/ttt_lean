dae4287a5aba626b77ad77b641e29fce2809ae71
Tony Gutierrez <pursual@gmail.com>
2015-12-08 12:24:12 -0500

    lean playing

--- Main/Controllers/GameController.js
+++ Main/Controllers/GameController.js
@@ -6,83 +6,24 @@ ticTacToe.controller('GameController', ['$scope', '$routeParams', '$location', '
 			case "human":
 				$scope.title = "Human Vs. Human";
 				$scope.directions = "When playing agaisnt another person you can use oldest person goes first, a coin toss, or just about anthing to determine who goes first. Either way, X goes first.";
-				break
+				break;
 
 			case "computer":
 				$scope.title = "Human Vs. Computer";
-				$scope.directions = "When playing agaisnt the computer, the human goes first. It's the only way to (mildly) entertain the computer. X goes first.'"
-				break
+				$scope.directions = "When playing agaisnt the computer, the human goes first. It's the only way to (mildly) entertain the computer. X goes first.'";
+				break;
 			default:
 				alert('That is not a valid URL.');
 				$location.path('/');
-				break
+				break;
 	}
 	
-	//Tic Tac Toe Cell Class
-	function cell(selected, id, player) {
-
-	  this.selected = selected;
-	  this.player = player;
-	  this.id = id;
-	  this.cellIcon = '';
-
-	  this.setSelected = function setSelected() {
-	    this.selected = true;
-	  }
-
-	  this.deselected = function deselected() {
-	    this.selected = false;
-	  }
-
-	  this.checkSelected = function checkSelected() {
-	    return this.selected;
-	  }
-
-	  this.setPlayer = function setPlayer(currentPlayer) {
-	    this.player = currentPlayer;
-	  }
-
-	  this.getPlayer = function getPlayer() {
-	    return this.player;
-	  }
-
-	}
-
-	//Called by checkWinner to make sure player matches in winning combonations.
-	function playerMatches(functionCell1, functionCell2, functionCell3){
-		//Checks if Players Match
-		var player1 = functionCell1.getPlayer(),
-			player2 = functionCell2.getPlayer(),
-			player3 = functionCell3.getPlayer();
-
-		if (player1 == player2 && player2 == player3 && player1 != null && player2 != null && player3 != null){
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	////Called by checkWinner to make sure cells are selected in winning combonations.
-	function cellsSelected(functionCell1, functionCell2, functionCell3){
-		//Checks if Cells are selected
-		var selectedCell1 = functionCell1.checkSelected(),
-		    selectedCell2 = functionCell2.checkSelected(),
-		    selectedCell3 = functionCell3.checkSelected();
-
-		if(selectedCell1 && selectedCell2 && selectedCell3){
-			return true;
-		} else {
-			return false;
-		}
-	}
-
 	//Called by CheckWinner and checks for draw.
 	function checkDraw(board){
-		if(board[0].checkSelected() && board[1].checkSelected() && board[2].checkSelected() && board[3].checkSelected() && board[4].checkSelected() && board[5].checkSelected() && board[6].checkSelected() && board[7].checkSelected() && board[8].checkSelected()){
-			return true;
-		} else {
-			return false;
+		for (var i = 0; i < 9; i++) {
+			if (!board[i]) {return false;}
 		}
+		return true;
 	}
 
 	//Checks for winner.
@@ -102,72 +43,36 @@ ticTacToe.controller('GameController', ['$scope', '$routeParams', '$location', '
 				"Diagonal"
 				- cell1, cell5, cell9
 				- cell3, cell5, cell7
-		*/ 
+		*/
+		var winners = [
+			[0,1,2],
+			[3,4,5],
+			[6,7,8],
+			[0,3,6],
+			[1,4,7],
+			[2,5,8],
+			[0,4,8],
+			[2,4,6]
+		];
 
-		//- cell1, cell2, cell3
-		if(cellsSelected(board[0], board[1], board[2]) && playerMatches(board[0], board[1], board[2])){
-			$scope.setWinner = board[0].getPlayer();
-			return true;
-		} 
-
-		//- cell4, cell5, cell6
-		if(cellsSelected(board[3], board[4], board[5]) && playerMatches(board[3], board[4], board[5])){
-			$scope.setWinner = board[3].getPlayer();
-			return true;
-		} 
-
-		//- cell7, cell8, cell9
-		if(cellsSelected(board[6], board[7], board[8]) && playerMatches(board[6], board[7], board[8])){
-			$scope.setWinner = board[6].getPlayer();
-			return true;
-		} 
-
-		//- cell1, cell4, cell7
-		if(cellsSelected(board[0], board[3], board[6]) && playerMatches(board[0], board[3], board[6])){
-			$scope.setWinner = board[0].getPlayer();
-			return true;
-		} 
-
-		//- cell2, cell5, cell8
-		if(cellsSelected(board[1], board[4], board[7]) && playerMatches(board[1], board[4], board[7])){
-			$scope.setWinner = board[1].getPlayer();
-			return true;
-		} 
-
-		//- cell3, cell6, cell9
-		if(cellsSelected(board[2], board[5], board[8]) && playerMatches(board[2], board[5], board[8])){
-			$scope.setWinner = board[2].getPlayer();
-			return true;
-		} 
-
-		//- cell1, cell5, cell9
-		if(cellsSelected(board[0], board[4], board[8]) && playerMatches(board[0], board[4], board[8])){
-			$scope.setWinner = board[0].getPlayer();
-			return true;
-		} 
-
-		//- cell3, cell5, cell7
-		if(cellsSelected(board[2], board[4], board[6]) && playerMatches(board[2], board[4], board[6])){
-			$scope.setWinner = board[2].getPlayer();
-			return true;
-		} 
+		for (var i = 0; i < winners.length; i++) {
+			var cells = winners[i];
+			if (board[cells[0]] && board[cells[0]] === board[cells[1]] && board[cells[1]] === board[cells[2]]) {$scope.setWinner = board[cells[0]];return true;}
+		}
+		return false;
 	}
 	
-	function makeMove(move, player, board){
+	function makeMove(move, player, board) {
+		if (board[move]) {return false;}
+
 		var newBoard = cloneBoard(board);
-		if(newBoard[move].checkSelected() == false){
-			newBoard[move].setSelected();
-			newBoard[move].setPlayer(player);
-			return newBoard;
-		} else {
-			return false;
-		}
+		newBoard[move] = player;
+		return newBoard;
 	}
 
 	//Clones board using angular merge and leaves current board in tack.
 	function cloneBoard(board){
-		var cloneBoard = angular.merge([], board);
-		return cloneBoard;
+		return angular.merge([], board);
 	}
 
 	function findMove(board){
@@ -197,7 +102,7 @@ ticTacToe.controller('GameController', ['$scope', '$routeParams', '$location', '
 		
 		if (checkWinner(board)){
 			
-			if ($scope.setWinner == "O"){
+			if ($scope.setWinner === "O"){
 				return 1;
 			} else {
 				return -1;
@@ -230,7 +135,7 @@ ticTacToe.controller('GameController', ['$scope', '$routeParams', '$location', '
 
 		if (checkWinner(board)){
 			
-			if ($scope.setWinner == "O"){
+			if ($scope.setWinner === "O"){
 				return 1;
 			} else {
 				return -1;
@@ -261,18 +166,16 @@ ticTacToe.controller('GameController', ['$scope', '$routeParams', '$location', '
 
 	function ticTacToeAi(){	
 	
-		var moveID = findMove(CurrentBoard);
+		var moveID = findMove($scope.board);
 		$scope.currentPlayer = 'X';
-		CurrentBoard[moveID].setSelected();
-		CurrentBoard[moveID].setPlayer('O');
-		CurrentBoard[moveID].cellIcon = '<i class="fa fa-circle-o   fa-4x cellIcon"></i>';
+		$scope.board[moveID] = 'O';
 		$scope.aiMessage = '';
-		if(checkWinner(CurrentBoard)){
+		if(checkWinner($scope.board)){
 			$scope.gameOver = true;
 			$scope.gameResult = "The computer has won.";
 			$("#myModal").modal();
 		} 
-		if (checkDraw(CurrentBoard)){
+		if (checkDraw($scope.board)){
 			$scope.gameOver = true;
 			$scope.gameResult = "The game ended in a draw.";
 			$("#myModal").modal();
@@ -281,95 +184,53 @@ ticTacToe.controller('GameController', ['$scope', '$routeParams', '$location', '
 	}
 
 	//This function handles switching players for both human and computer modes.
-	function switchPlayer(functionCell){
+	function switchPlayer(){
 		
 		if($routeParams.who == 'computer' && $scope.currentPlayer == 'X' && $scope.gameOver === false){
-			functionCell.cellIcon = '<i class="fa fa-times  fa-4x cellIcon"></i>';
 			$scope.currentPlayer = 'O';
 			$scope.aiMessage = 'The computer is thinking: ' + '<i class="fa fa-circle-o-notch fa-spin"></i>';
 			//placing AI logic here. 
 			$timeout(function () {
 				ticTacToeAi();
-			}, 1000);
+			}, 100);
 		} else {
 			
 			if($scope.currentPlayer == 'X' && $scope.gameOver === false){
 				$scope.currentPlayer = 'O';
-				functionCell.cellIcon = '<i class="fa fa-times  fa-4x cellIcon"></i>';
 			} else {
 				$scope.currentPlayer = 'X';
-				functionCell.cellIcon = '<i class="fa fa-circle-o   fa-4x cellIcon"></i>';
 			}
 		}
 
 	}
 
+	$scope.resetGame = function (){
+		$scope.board = [false, false, false, false, false, false, false, false, false];
+
+		$scope.currentPlayer = 'X'; 
+		$scope.gameOver = false;
+	};
+
 	//current variable setup
-	$scope.currentPlayer = 'X';
-	$scope.gameOver = false;
-	var cell1 = new cell(false, 1, null),
-	    cell2 = new cell(false, 2, null),
-	    cell3 = new cell(false, 3, null),
-	    cell4 = new cell(false, 4, null),
-	    cell5 = new cell(false, 5, null),
-	    cell6 = new cell(false, 6, null),
-	    cell7 = new cell(false, 7, null),
-	    cell8 = new cell(false, 8, null),
-	    cell9 = new cell(false, 9, null);
-	
-	var CurrentBoard = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9];
-	$scope.cell1 = cell1;
-	$scope.cell2 = cell2;
-	$scope.cell3 = cell3;
-	$scope.cell4 = cell4;
-	$scope.cell5 = cell5;
-	$scope.cell6 = cell6;
-	$scope.cell7 = cell7;
-	$scope.cell8 = cell8;
-	$scope.cell9 = cell9;
+	$scope.resetGame();
 
 	//handles whether a cell has been clicked
 	$scope.cellClick = function (cellNumber) {
-		var cell = $scope['cell' + cellNumber];
-		cell.setSelected();
-		cell.setPlayer($scope.currentPlayer);
-		if (checkWinner(CurrentBoard)){
+		if ($scope.board[cellNumber] || $scope.currentPlayer !== 'X') {return;}
+		$scope.board[cellNumber] = $scope.currentPlayer;
+
+		if (checkWinner($scope.board)){
 			$scope.gameOver = true;
 			$scope.gameResult = $scope.currentPlayer + " has won the game.";
 			$("#myModal").modal();
 		}
-		if (checkDraw(CurrentBoard)){
+		if (checkDraw($scope.board)){
 			$scope.gameOver = true;
 			$scope.gameResult = "The game ended in a draw.";
 			$("#myModal").modal();
 		}
-		switchPlayer(cell);
+		switchPlayer();
 	};
-
-	$scope.resetGame = function (){
-		cell1 = new cell(false, 1, null),
-	    cell2 = new cell(false, 2, null),
-	    cell3 = new cell(false, 3, null),
-	    cell4 = new cell(false, 4, null),
-	    cell5 = new cell(false, 5, null),
-	    cell6 = new cell(false, 6, null),
-	    cell7 = new cell(false, 7, null),
-	    cell8 = new cell(false, 8, null),
-	    cell9 = new cell(false, 9, null);
-	    CurrentBoard = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9];
-	    $scope.cell1 = cell1;
-		$scope.cell2 = cell2;
-		$scope.cell3 = cell3;
-		$scope.cell4 = cell4;
-		$scope.cell5 = cell5;
-		$scope.cell6 = cell6;
-		$scope.cell7 = cell7;
-		$scope.cell8 = cell8;
-		$scope.cell9 = cell9;
-		$scope.currentPlayer = 'X'; 
-		$scope.gameOver = false;
-	}
-
 
 	
 }
